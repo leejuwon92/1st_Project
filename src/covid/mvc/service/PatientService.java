28e8b95af2f44d6b538df543a03de27e7eec946d
KOSTA <KOSTA@192.168.0.108>
2020-09-15 12:25:01 +0900

    patient service

--- src/covid/mvc/service/PatientService.java
+++ src/covid/mvc/service/PatientService.java
@@ -1,8 +1,32 @@
 package covid.mvc.service;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import covid.mvc.dao.PatientDAO;
 import covid.mvc.dao.PatientDAOImpl;
+import covid.mvc.dto.Hospital;
+import covid.mvc.dto.Patient;
+import covid.mvc.dto.Route;
 
 public class PatientService {
 	PatientDAO patientDAO = new PatientDAOImpl();
+	
+	public Hospital selectHospitalByAddr(String userAddr) throws SQLException {
+		Hospital hospital = patientDAO.selectHospitalByAddr(userAddr);
+		return hospital;
+	}
+	
+	public List<Hospital> selectHospitalAll() throws SQLException {
+		List<Hospital> list = patientDAO.selectHospitalAll();
+		return list;
+	}
+	
+	public void insertPatient(Patient patient) {
+		
+	}
+	
+	public void insertRoute(Route route) {
+		
+	}
 }
